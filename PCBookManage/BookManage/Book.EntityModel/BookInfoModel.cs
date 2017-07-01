443070d07a2f946bf72cec2185bd98d63cbcb721
Linda201411 <1625759939@qq.com>
2017-07-01 06:33:59 -0700

    add entity model

---
+++ PCBookManage/BookManage/Book.EntityModel/BookInfoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.EntityModel
+{
+    public class BookInfoModel
+    {
+        public long Id { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public DateTime? PublicDate { get; set; }
+        public string PublicAddress { get; set; }
+        public int? CategoryId { get; set; }
+        public string Remark { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string CreateBy { get; set; }
+    }
+}
---
+++ PCBookManage/BookManage/Book.EntityModel/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.EntityModel
+{
+    public class Operate {
+        public int Status { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ViewResult<T> {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+
+    public class MuliResult<T> {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public List<T> Datas { get; set; }
+        public int Total { get; set; }
+    }
+}
