fea8e3dda2e1d415cc88a16e2489ce8a466bf439
Linda201411 <1625759939@qq.com>
2017-07-10 16:13:19 -0700

    add book collection to the system

---
+++ PCBookManage/BookManage/Book.Business/BookCollectionBusiness.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Book.Business.ConvertModel;
+using Book.DataAccess;
+using Book.EntityModel;
+
+namespace Book.Business
+{
+    public class BookCollectionBusiness
+    {
+        private readonly BookCollectionAgent _bookCollectionAgent; 
+
+        public BookCollectionBusiness()
+        {
+            _bookCollectionAgent = new BookCollectionAgent();
+        }
+
+        public async Task<MuliResult<BookCollectionModel>> GetCollectionList(int userId)
+        {
+            var result=new MuliResult<BookCollectionModel>();
+            try
+            {
+                var datalist = await _bookCollectionAgent.GetCollectList(userId);
+                result.Datas = datalist.Select(x => x.ToBookCollectionModel()).ToList();
+            }
+            catch (Exception ex)
+            {
+                result.Status = -1;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ PCBookManage/BookManage/Book.WebApi/Controllers/BookCollectionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Book.Business;
+using Book.EntityModel;
+
+namespace Book.WebApi.Controllers
+{
+    [RoutePrefix("api/collect")]
+    public class BookCollectionController : ApiController
+    {
+        private readonly BookCollectionBusiness _bookCollectionBusiness;
+
+        public BookCollectionController()
+        {
+            this._bookCollectionBusiness=new BookCollectionBusiness();
+        }
+
+        [Route("getCollectList/{userId}")]
+        [HttpGet]
+        public async Task<MuliResult<BookCollectionModel>> GetCollectList(int userId)
+        {
+            return await _bookCollectionBusiness.GetCollectionList(userId);
+        }
+    }
+}
