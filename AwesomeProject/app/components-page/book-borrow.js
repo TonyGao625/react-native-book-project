3b38acfccf6d1d1c3089a4e520654480f5f67033
Linda201411 <1625759939@qq.com>
2017-07-10 18:07:40 -0700

    delete book bootem ui at book borrow page

--- AwesomeProject/app/components-page/book-borrow.js
+++ AwesomeProject/app/components-page/book-borrow.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView} from 'react-native';
 import Borrow from './../compponents-template/book-borrow'
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import FormButton from '../components-smart/button';
 
 export default class BookBorrow extends Component {
   static navigationOptions = {
@@ -21,49 +22,10 @@ export default class BookBorrow extends Component {
     return (
       <ScrollView>
         <Borrow navigation={this.props.navigation} />
-         <View style={styles.operation}>
-         <FormButton style={styles.operateItem} 
-         onPress={this._selectAll}
-         title='全选'></FormButton>
-         <FormButton style={styles.operateItem} 
-         onPress={this._unSelectAll}
-         title='撤销'></FormButton>
-         <FormButton style={styles.operateItem} 
-         title='确定'></FormButton>
-       </View>
       </ScrollView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  item: {
-    flex: 1,
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    height: 40,
-    paddingTop: 10,
-    paddingBottom: 10,
-    marginLeft: 15,
-    marginRight: 15
-  },
-  statusIcon: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end'
-  },
-  icon: {
-    marginRight: 10
-  },
-  operation:{
-    flex: 1,
-    flexDirection: 'row',
-    margin:10
-  },
-  operateItem:{
-    marginRight: 10
-  }
-});
 
 
