f471fc14917548deaea6d7d50ecc7f475d2f06c2
Linda201411 <1625759939@qq.com>
2017-07-11 18:04:56 -0700

    change search item from all to book all page

--- AwesomeProject/app/components-page/book-all.js
+++ AwesomeProject/app/components-page/book-all.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { AsyncStorage, ScrollView, StyleSheet } from 'react-native';
+import { AsyncStorage, ScrollView, StyleSheet, View, TouchableHighlight} from 'react-native';
 import BookList from './../compponents-template/book-all'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import MainAdd from './../compponents-template/main-add';
+import FormTextInput from '../components-smart/text-input'
+
+
 export default class BookAll extends Component {
   static navigationOptions = ({ navigation }) => ({
     tabBarLabel: '全部',
@@ -13,9 +16,28 @@ export default class BookAll extends Component {
         style={[{ tintColor: tintColor }]}
         color='white' />
     ),
-    headerRight: (
-      <MainAdd navigation={navigation} />
-    )
+    headerTitle: <View style={{
+            flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop:10,
+                marginBottom:-10
+          }}>
+                  <Icon 
+                  style={{marginLeft:10, marginBottom:20}}
+                  name="crop-free" 
+                  size={25}
+                  color='#43A047'/>
+                  <FormTextInput 
+                  placeholder='search'></FormTextInput>
+      </View>,
+   headerRight: <TouchableHighlight>
+        <Icon 
+          style={{marginRight:10}}
+          name="dehaze" 
+          size={20}/>
+     </TouchableHighlight>
   });
 
   componentWillMount() {
--- AwesomeProject/app/components-page/route.js
+++ AwesomeProject/app/components-page/route.js
@@ -5,7 +5,7 @@ import Main from './main';
 import Account from './account';
 import BookAdd from './book-add';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import FormTextInput from '../components-smart/text-input'
+
 
 export default class Route extends Component {
   constructor(props) {
@@ -18,37 +18,6 @@ export default class Route extends Component {
   }
 }
 
-const mainNavigationOptions = ({ navigation }) => ({
-  headerTitle: <View style={{
-    flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop:10,
-        marginBottom:-10
-  }}>
-                  <Icon 
-                  style={{marginLeft:10, marginBottom:20}}
-                  name="crop-free" 
-                  size={25}
-                  color='#43A047'/>
-                  <FormTextInput 
-                  placeholder='search'></FormTextInput>
-  </View>,
-  
-   headerRight: <TouchableHighlight>
-     <Icon 
-                  style={{marginRight:10}}
-                  name="dehaze" 
-                  size={20}/>
-     </TouchableHighlight>,
-  //   <MainAdd navigation={navigation}/>,
-  // headerStyle:{
-  //   paddingRight:10
-  // }
-  headerLeft: null
-});
-
 const RouteItem = StackNavigator({
   Account: {
     screen: Account,
@@ -57,8 +26,7 @@ const RouteItem = StackNavigator({
     }
   },
   Main: {
-    screen: Main,
-    navigationOptions: mainNavigationOptions
+    screen: Main
   },
   BookAdd: {
     screen: BookAdd
