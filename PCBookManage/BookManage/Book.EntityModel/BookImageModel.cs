c2a39c657513ed79c2e6a925475b2dfcaa34e085
Linda201411 <1625759939@qq.com>
2017-07-13 15:28:34 -0700

    add book image model to the web API

---
+++ PCBookManage/BookManage/Book.EntityModel/BookImageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.EntityModel
+{
+    public class BookImageModel
+    {
+        public long Id { get; set; }
+        public long BookId { get; set; }
+        public bool IsCover { get; set; }
+        public int SortOrder { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
