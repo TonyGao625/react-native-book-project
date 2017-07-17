feb141ab8e059a4c5ad4800cba83d68ad3294751
Linda201411 <1625759939@qq.com>
2017-07-17 14:56:20 -0700

    disable warning

--- AwesomeProject/app/app.js
+++ AwesomeProject/app/app.js
@@ -6,6 +6,7 @@ import store from './store';
 
 export default class App extends Component {
   render() {
+    console.disableYellowBox = true;
     return (
         <Provider store={store}>
             <Route/>
