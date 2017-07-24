35d3da16261376921145a61a1b732be33561ad83
gao <784627606@qq.com>
2017-07-24 21:17:28 +0800

    refactor the route and add permission

--- AwesomeProject/app/route.js
+++ AwesomeProject/app/route.js
@@ -5,46 +5,62 @@ import AccountScreen from './screens/account-screen';
 import BookAddScreen from './screens/book-add-screen';
 import BookDetailScreen from './screens/book-detail-screen';
 import SearchScreen from './screens/book-search-screen';
+import { connect } from 'react-redux'
+@connect((store) => {
+  return {
+    IsAuthened: store.accountReducer.permission.IsAuthened,
+  }
+})
 
 export default class Route extends Component {
   constructor(props) {
     super(props);
   }
   render() {
+    const RouteItem = CreateRootNavigator(this.props.IsAuthened)
     return (
       <RouteItem />
     );
   }
 }
-
-const RouteItem = StackNavigator({
-  Account: {
-    screen: AccountScreen,
-    navigationOptions: {
-      header: null
-    }
-  },
-  Main: {
-    screen: MainScreen,
-    navigationOptions: {
-      headerLeft:null
-    }
-  },
-  BookAdd: {
-    screen: BookAddScreen
-  },
-  BookDetail:{
-    screen:BookDetailScreen
-  },
-  Search:{
-    screen:SearchScreen
+const CreateRootNavigator = (IsAuthened = false) => {
+  var initialRouteName = 'Main';
+  if (IsAuthened) {
+    initialRouteName = 'Main'
+  } else {
+    initialRouteName = 'Account'
   }
-}, {
-    cardStyle: {
-      backgroundColor: 'white'
+
+  return StackNavigator({
+    Account: {
+      screen: AccountScreen,
+      navigationOptions: {
+        header: null
+      }
     },
-    initialRouteName: 'Main',
-    //headerMode:'none'
-  });
+    Main: {
+      screen: MainScreen,
+      navigationOptions: {
+        headerLeft: null
+      }
+    },
+    BookAdd: {
+      screen: BookAddScreen
+    },
+    BookDetail: {
+      screen: BookDetailScreen
+    },
+    Search: {
+      screen: SearchScreen
+    }
+  }, {
+      cardStyle: {
+        backgroundColor: 'white'
+      },
+      initialRouteName: initialRouteName,
+      //headerMode:'none'
+    });
+}
+
 
 
