99864270770a00ace8c6f116e44756236c645894
Linda201411 <1625759939@qq.com>
2017-07-26 18:45:32 +0800

    modify style at no data

--- AwesomeProject/app/components-cell/book-nodata.js
+++ AwesomeProject/app/components-cell/book-nodata.js
@@ -4,7 +4,9 @@ import { Text } from 'react-native';
 export default class BookNoData extends Component {
   render() {
     return (
-      <Text style={{marginLeft:10, marginTop:10}}>没有数据</Text>
+      <Text style={{marginLeft:10, marginTop:10,color:'red', textAlign:'center'}}>
+        没有数据 !
+      </Text>
     );
   }
 }
