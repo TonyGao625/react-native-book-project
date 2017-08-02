f7984dfa21cd19f367b186dd7e201f74d13f0464
Linda201411 <1625759939@qq.com>
2017-08-01 17:03:51 -0700

    当点击点击还书页面右边文字时改变内容

--- AwesomeProject/app/screens/book-borrow.js
+++ AwesomeProject/app/screens/book-borrow.js
@@ -3,6 +3,29 @@ import { View, TouchableOpacity, Text } from 'react-native';
 import BookList from './../compponents-template/book-borrow'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+class EditText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '编辑',
+    };
+  }
+  _changeEdit=()=>{
+    this.setState({
+       text: '完成'
+    });
+  }
+  render() {
+    var navigation=this.props.navigation;
+    return (
+      <TouchableOpacity onPress={this._changeEdit}>
+        <Text style={{ color: "black", fontWeight: "bold", fontSize: 16, paddingRight: 10 }} >{this.state.text}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+
 export default class BookBorrow extends Component {
   static navigationOptions = {
     tabBarLabel: '借阅单',
@@ -10,11 +33,7 @@ export default class BookBorrow extends Component {
       name="library-books"
       size={20}
       color='white' />,
-    headerRight:
-    <TouchableOpacity onPress={() => { navigation.navigate('Checkout'); }}
-    >
-      <Text style={{ color: "black", fontWeight: "bold", fontSize: 16, paddingRight: 10 }} >编辑</Text>
-    </TouchableOpacity>
+    headerRight: <EditText/>
   };
   render() {
     return (
