89cc2bd3bd1bf704a4928509cd8af46e12283691
Linda201411 <1625759939@qq.com>
2017-08-02 18:23:48 -0700

    新增添加图片功能

--- AwesomeProject/app/compponents-template/system-manage.js
+++ AwesomeProject/app/compponents-template/system-manage.js
@@ -11,30 +11,73 @@ import {
 } from 'react-native';
 import { StackNavigator, TabNavigator } from 'react-navigation'
 import FormButton from './../components-cell/form-button'
+import { ImagePicker } from 'antd-mobile';
+
+const data = [{
+  url: 'https://zos.alipayobjects.com/rmsportal/PZUUCKTRIHWiZSY.jpeg',
+  id: '2121',
+}, {
+  url: 'https://zos.alipayobjects.com/rmsportal/hqQWgTXdrlmVVYi.jpeg',
+  id: '2122',
+}];
 
 export default class SystemManage extends Component {
-  _addBookInfo=()=>{
+  _addBookInfo = () => {
     const { navigate } = this.props.navigation;
-      navigate('BookAdd')
+    navigate('BookAdd')
   }
-  _takePhoto=()=>{
+  _takePhoto = () => {
     const { navigate } = this.props.navigation;
     navigate('TakePhoto')
   }
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      files: data,
+    };
+  }
+  onChange = (files, type, index) => {
+    debugger;
+    this.setState({
+      files,
+    });
+  };
+  // onAddImageClick = (e) => {
+  //   e.preventDefault();
+  //   this.setState({
+  //     files: this.state.files.concat({
+  //       url: 'https://zos.alipayobjects.com/rmsportal/hqQWgTXdrlmVVYi.jpeg',
+  //       id: '3',
+  //     }),
+  //   });
+  // };
+  onTabChange = (key) => {
+    console.log(key);
+  };
   render() {
+    const { files } = this.state;
     return (
       <ScrollView>
-           {/* <FormButton 
+        {/* <FormButton 
               title='添加图书信息' 
               onPress={this._addBookInfo}
           /> */}
 
-          <FormButton 
-              title='拍照' 
-              onPress={this._takePhoto}
-          />
+        <FormButton
+          title='拍照'
+          onPress={this._takePhoto}
+        />
+
+        <ImagePicker
+          files={files}
+          onChange={this.onChange}
+          onImageClick={(index, fs) => console.log(index, fs)}
+          selectable={files.length < 5}
+          onAddImageClick={this.onAddImageClick}
+        />
       </ScrollView>
-  );
+    );
   }
 }
 
